32588590be7c3e73009a1613019e707f01e22c22
aerdogan <tuxposta@gmail.com>
2021-05-06 10:48:33 +0300

    test

--- nLayeredDemo/src/Main.java
+++ nLayeredDemo/src/Main.java
@@ -7,9 +7,11 @@ import entities.concretes.Product;
 public class Main {
 
 	public static void main(String[] args) {
+
 		ProductService productService = new ProductManager(new HibernateProductDao(), new JLoggerManagerAdapter());
 
 		Product product = new Product(1, 2, "Elma", 12, 50);
+
 		productService.add(product);
 	}
 
